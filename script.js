d5985a7c8d88335ea9220ac65aa77944c7d630bb
SBassPurdue <54543055+SBassPurdue@users.noreply.github.com>
2019-09-15 06:33:41 -0400

    Bird is good boye

--- script.js
+++ script.js
@@ -12,24 +12,52 @@ function renderTree(score) {
   }
 }
 
+var birdLocations = [[275, 0], [25, 0], [5, 380], [295, 380]];
+document.addEventListener("DOMContentLoaded", birdMovementInit());
+
+function birdMovementInit() {
+  console.log("Bird Move Init");
+  var birdMovement = setInterval(birdMovementRoutine, 7000);
+}
+
+function birdMovementRoutine() {
+  console.log("Bird Move");
+  var rand = Math.floor(4 * Math.random());
+  moveBird(birdLocations[rand][0], birdLocations[rand][1]);
+}
+
 //Centered at top left of bird
 function moveBird(newX, newY) {
-  var bird = document.getElementById("birdBox");
-  var oldLeft = parseInt(bird.style.left, 10);
-  var oldTop = parseInt(bird.style.top, 10);
-  var duration = 1500; //ms
-  var deltaT = 10;
+  var birdBox = document.getElementById("birdBox");
+  var birdSprite = document.getElementById("birdSprite");
+  var oldLeft = parseInt(birdBox.style.left, 10);
+  var oldTop = parseInt(birdBox.style.top, 10);
+  var duration = 500; //ms
+  var deltaT = 8;
   var deltaX = (newX - oldLeft) / duration; //pixels per time step
   var deltaY = (newY - oldTop) / duration; //pixels per time step
+  var birdDown = (deltaX >= 0 ? "BirdFlapDown.png" : "BirdFlapDownRev.png");
+  var birdStill = (deltaX >= 0 ? "BirdStill.png" : "BirdStillRev.png");
+  var birdUp = (deltaX >= 0 ? "BirdFlapUp.png" : "BirdFlapUpRev.png");
   var t = 0;
+  var i = 0;
 
   var movement = setInterval(function() {
-    bird.style.left = (parseFloat(bird.style.left) + (deltaX * deltaT)) + "px";
-    bird.style.top = (parseFloat(bird.style.top) + (deltaY * deltaT)) + "px";
+    birdBox.style.left = (parseFloat(birdBox.style.left) + (deltaX * deltaT)) + "px";
+    birdBox.style.top = (parseFloat(birdBox.style.top) + (deltaY * deltaT)) + "px";
     t = t + deltaT;
+    i++;
+    if (i % 16 <= 4) {
+      birdSprite.src = birdDown;
+    } else if (i % 16 <= 8 || i % 16 > 12) {
+      birdSprite.src = birdStill;
+    } else {
+      birdSprite.src = birdUp;
+    }
     if (t > duration) {
-      bird.style.left = newX + "px";
-      bird.style.top = newY + "px";
+      birdBox.style.left = newX + "px";
+      birdBox.style.top = newY + "px";
+      birdSprite.src = birdStill;
       clearInterval(movement);
     }
   }, deltaT);
